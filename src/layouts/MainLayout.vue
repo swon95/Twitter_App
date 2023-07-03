8cbdcc816e613c67d79df50424a02ac60f725d1e
swon95 <shimtkddnjs13@gmail.com>
2023-07-03 19:27:12 +0900

    [FEAT] Left Layout_Navigation

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <q-layout view="lHr lpR fFf">
 
+    <!-- 중앙 -->
     <q-header bordered class="bg-white text-black">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
@@ -12,10 +13,45 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+    <!-- 왼쪽 -->
+    <q-drawer 
+      v-model="leftDrawerOpen" 
+      side="left" 
+      :width="283"
+      bordered 
+      show-if-above>
+      <!-- 4 방향으로 중간 량의 padding 추가 -> q-pa-md-->
+      <q-icon class="q-pa-md"
+        name="fas fa-dove"
+        size="lg"
+        color="primary" />
+
       <!-- drawer content -->
+
+      <!-- bordered -> 테두리 -->
+      <q-list>
+      
+      <!-- 루트 경로로 지정 => / -->
+      <q-item to="/" clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon name="home" size="md"/>
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+      </q-item>
+
+      <q-item to="/about" clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon name="help" size="md"/>
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+      </q-item>
+      
+    </q-list>
     </q-drawer>
 
+    <!-- 오른쪽 -->
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
       <!-- drawer content -->
     </q-drawer>
