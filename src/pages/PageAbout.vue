577ce56bdb302574d54dc91e09848f77e957fb0a
swon95 <shimtkddnjs13@gmail.com>
2023-07-04 18:32:44 +0900

    [FEAT] New Tweet Form

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,10 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About</h4>
+    <div class="text-body1">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error minus inventore ducimus iusto nostrum sequi asperiores maxime incidunt neque harum vero voluptas et voluptates, beatae sapiente, id hic debitis non!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas adipisci eius dolores in qui laudantium cumque, perspiciatis, earum quis magnam eveniet eaque velit neque, quisquam nulla et. Sapiente, unde doloribus.</p>
+    </div>
   </q-page>
 </template>
 
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,45 @@
 <template>
   <q-page>
-    <h5>Home</h5>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <!-- 문자열 카운터를 위한 newTwitterContent -->
+      <div class="col">
+        <q-input 
+          class="new-tweet"
+            bottom-slots 
+            v-model="newTwitterContent" 
+            label="What's happening?!" 
+            counter 
+            maxlength="280" 
+            :dense="dense"
+            autogrow
+          >
+            <template v-slot:before>
+              <q-avatar>
+                <img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/43583/elephant-emoji-clipart-md.png">
+              </q-avatar>
+            </template>
+        </q-input>
+      </div>
+      
+      <div class="col col-shrink">
+        <q-btn
+          class="q-mb-lg"
+          :disabled="!newTwitterContent"
+          unelevated 
+          rounded 
+          color="primary" 
+          label="Tweet"
+          no-caps
+         />
+      </div>
+    </div>
+
+    <q-separator 
+      class="divider" 
+      color="grey-2" 
+      size="10px" 
+    />
+    
   </q-page>
 </template>
 
@@ -8,6 +47,23 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'PageHome'
+  name: 'PageHome',
+  data() {
+    return {
+      newTwitterContent: ''
+    }
+  }
 })
 </script>
+
+<style lang="sass">
+.new-tweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
+</style>
