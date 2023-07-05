f1fc72e21539a5957284abacd03ea1a26d3fbf3a
swon95 <shimtkddnjs13@gmail.com>
2023-07-06 01:07:11 +0900

    [FEAT] Animation & List UI

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,85 +1,95 @@
 <template>
-  <q-page>
-    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
-      <!-- 문자열 카운터를 위한 newQweetContent -->
-      <div class="col">
-        <q-input 
-          class="new-qweet"
-            bottom-slots 
-            v-model="newQweetContent" 
-            label="What's happening?!" 
-            counter 
-            maxlength="280" 
-            :dense="dense"
-            autogrow
-          >
-            <template v-slot:before>
-              <q-avatar>
-                <img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/43583/elephant-emoji-clipart-md.png">
-              </q-avatar>
-            </template>
-        </q-input>
+  <q-page class="relative-position">
+    <q-scroll-area class="absolute fullscreen">
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+        <!-- 문자열 카운터를 위한 newQweetContent -->
+        <div class="col">
+          <q-input 
+              v-model="newQweetContent" 
+              class="new-qweet"
+              label="What's happening?!" 
+              maxlength="280"
+              bottom-slots 
+              counter 
+              autogrow
+            >
+              <template v-slot:before>
+                <q-avatar>
+                  <img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/43583/elephant-emoji-clipart-md.png">
+                </q-avatar>
+              </template>
+          </q-input>
+        </div>
+        
+        <div class="col col-shrink">
+          <!-- @click => 클릭이벤트 추가 -->
+          <q-btn
+            @click="addNewQweet"
+            class="q-mb-lg"
+            :disabled="!newQweetContent"
+            unelevated 
+            rounded 
+            color="primary" 
+            label="Qweet"
+            no-caps
+           />
+        </div>
       </div>
-      
-      <div class="col col-shrink">
-        <!-- @click => 클릭이벤트 추가 -->
-        <q-btn
-          @click="addNewQweet"
-          class="q-mb-lg"
-          :disabled="!newQweetContent"
-          unelevated 
-          rounded 
-          color="primary" 
-          label="Qweet"
-          no-caps
-         />
-      </div>
-    </div>
-
-    <!-- 영역 구분 -->
-    <q-separator 
-      class="divider" 
-      color="grey-2" 
-      size="10px" 
-    />
-
-    <q-list >
-      <!-- v-for (루프) 메소드를 통해 페이지에 데이터를 뿌려줌 -->
-      <!-- 현재 date 를 key 로 -->
-      <q-item
-       class="q-py-md" 
-       v-for="qweet in qweets" 
-       :key="qweet.date"
-       >
-        <q-item-section avatar top>
-          <q-avatar size="xl">
-            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label class="text-subtitle1">
-            <strong>New User !</strong>
-            <span class="text-grey-7"> @새로운 사용자</span>
-          </q-item-label>
-          <q-item-label class="qweet-content text-body1">
-            {{ qweet.content }}
-          </q-item-label>
-
-          <div class="qweet-icons row justify-between">
-            <q-btn flat round size="sm" color="grey" icon="far fa-comment" />
-            <q-btn flat round size="sm" color="grey" icon="fas fa-retweet" />
-            <q-btn flat round size="sm" color="grey" icon="far fa-heart" />
-            <q-btn @click="deleteQweet(qweet)" flat round size="sm" color="grey" icon="fas fa-trash" />
-          </div>          
-        </q-item-section>
-
-        <q-item-section side top>
-          {{ qweet.date | relativeDate }}
-        </q-item-section>
-      </q-item>
-    </q-list>
-
+    
+      <!-- 영역 구분 -->
+      <q-separator 
+        class="divider" 
+        color="grey-2" 
+        size="10px" 
+      />
+    
+      <q-list separator>
+        <transition-group
+          appear
+          enter-active-class="animated fadeIn"
+          leave-active-class="animated fadeOut"
+        >
+        <!-- v-for (루프) 메소드를 통해 페이지에 데이터를 뿌려줌 -->
+        <!-- 현재 date 를 key 로 -->
+        <q-item
+         class="qweet q-py-md" 
+         v-for="qweet in qweets" 
+         :key="qweet.date"
+         >
+          <q-item-section avatar top>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+            </q-avatar>
+          </q-item-section>
+        
+          <q-item-section>
+            <q-item-label class="text-subtitle1">
+              <strong>New User !</strong>
+              <span class="text-grey-7">
+                 @새로운 사용자
+                <br class="lt-md">
+                 &bull; {{ qweet.date | relativeDate }}
+              </span>
+            </q-item-label>
+            <q-item-label class="qweet-content text-body1">
+              {{ qweet.content }}
+            </q-item-label>
+          
+            <div class="qweet-icons row justify-between">
+              <q-btn flat round size="sm" color="grey" icon="far fa-comment" />
+              <q-btn flat round size="sm" color="grey" icon="fas fa-retweet" />
+              <q-btn flat round size="sm" color="grey" icon="far fa-heart" />
+              <q-btn @click="deleteQweet(qweet)" flat round size="sm" color="grey" icon="fas fa-trash" />
+            </div>          
+          </q-item-section>
+        
+          <q-item-section side top>
+            
+          </q-item-section>
+        </q-item>
+        </transition-group>
+      </q-list>
+    </q-scroll-area>
   </q-page>
 </template>
 
@@ -118,6 +128,7 @@ export default {
         date: Date.now()
       }
       this.qweets.unshift(newQweet) // push, unshift
+      this.newQweetContent = ''
     },
     deleteQweet(qweet) {
       // console.log('Delete Tweet: ', qweet)
@@ -143,6 +154,9 @@ export default {
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-4
+
+.qweet:not(:first-child)
+  border-top: 1px solid rgba(0, 0, 0, 0.12)
 
 .qweet-content
   padding-top: 5px
